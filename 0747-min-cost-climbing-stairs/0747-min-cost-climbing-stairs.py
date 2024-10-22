d1fbe1b5de474f39daf7dd0db3e82853893d56ff
Pavan Shanbhag <pavanshanbhag12@gmail.com>
2024-10-22 03:17:49 -0700

    Time: 2 ms (99.21%), Space: 16.8 MB (20.99%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -3,13 +3,14 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         dp = defaultdict(int)
         n = len(cost)
+
+        # Base Case
+        if n < 2:
+            return cost[n-1] 
+    
         cost.append(0)
-        
-        dp[n] = 0
         dp[0] = cost[0]
-        if n <= 1:
-            return cost[0] 
-        dp[1] = min(cost[1], dp[0]+cost[1]) 
+        dp[1] = cost[1]
 
         for i in range(2,n+1):
             dp[i] = cost[i] + min(dp[i-1],dp[i-2])
