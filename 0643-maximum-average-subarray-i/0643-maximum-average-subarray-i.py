2e68443ebf82902d8e4d3b4b87309c92c27a9ef1
Pavan Shanbhag <pavanshanbhag12@gmail.com>
2024-11-03 00:54:15 -0700

    Time: 116 ms (15.09%), Space: 26.3 MB (98.16%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        max_sum, window_avg, window_sum, window_start = float('-inf'), 0, 0, 0
+        max_avg, window_avg, window_sum, window_start = float('-inf'), 0, 0, 0
         
         # Edge case - n < k : no need to handle as constraint clearly specifies that k <= n
         # Edge case - empty string : no need to handle
@@ -10,10 +10,10 @@ class Solution:
 
             # slide the window when size is reached to k 
             if window_end >= k-1 : 
-                #window_avg =  window_sum/k
-                max_sum = max(max_sum, window_sum)
+                window_avg =  window_sum/k
+                max_avg = max(max_avg, window_avg)
                 window_sum -= nums[window_start] # remove the element going out
                 window_start += 1 # slide the window ahead
         
-        return max_sum/k
+        return max_avg
                 
