1a3e06602935bf501971aa8dc4c07022d1baddf7
Pavan Shanbhag <pavanshanbhag12@gmail.com>
2024-11-06 00:43:19 -0800

    Time: 13 ms (5.83%), Space: 17.9 MB (60.43%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -14,23 +14,6 @@ class Solution:
             profit = max(buy, dont_buy)
         return profit 
 
-    def f_memo(self, idx, stock, prices, dp):
-        # Base Case 
-        if idx == len(prices):
-            return 0
-        if dp[idx][stock] != -1:
-            return dp[idx][stock]
-        if stock:
-            sell = prices[idx] + dp[idx+1][0]
-            dont_sell = 0 +  dp[idx+1][1] 
-            dp[idx][stock] = max(sell, dont_sell)
-        else:
-            buy = -prices[idx] + dp[idx+1][1]
-            dont_buy = 0 + dp[idx+1][0]
-            dp[idx][stock] = max(buy, dont_buy)
-
-        return dp[idx][stock] 
-
     def maxProfit_tab(self, prices: List[int]) -> int:
         n = len(prices)
         dp = [[-1 for i in range(2)] for _ in range(n+1)]
